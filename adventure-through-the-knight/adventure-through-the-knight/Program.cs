ceb112c539624ccc85de8d53f54e33e1e8017b89
Justin Walrath <walrathjaw@gmail.com>
2013-01-08 21:52:53 -0500

    initial push for Adventure Through The Knight game using XNA and monogame

---
+++ adventure-through-the-knight/adventure-through-the-knight/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace adventure_through_the_knight
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
