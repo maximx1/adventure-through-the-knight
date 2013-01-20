40ac02baff31f2fbcf80561ae16f86cc03a9d9b5
palmern22 <palmern22@gmail.com>
2013-01-19 23:36:21 -0500

    Created a static collision detection class, added a wall manager class, a wall class, and an NPC class.

--- adventure-through-the-knight/adventure-through-the-knight/Output/Base/Sprite.cs
+++ adventure-through-the-knight/adventure-through-the-knight/Output/Base/Sprite.cs
@@ -30,6 +30,7 @@ namespace adventure_through_the_knight.Output
         private Direction SpriteDirection;                          //The direction the Sprite is facing.
         private Direction LastDirection;                            //The Last direction that the sprite was walking.
         private Direction MovementDirection;                        //The direction of motion.
+        private Walls.WallManager wallManager;                       // wall manager for collision detection
         
         protected Dictionary<Direction, int> spriteSheetRows;       //Dictionary holding the different animations of the character.
         protected Vector2 Velocity { get; set; }                    //The direction of the sprite.
@@ -109,7 +110,7 @@ namespace adventure_through_the_knight.Output
 		/// <param name='spriteBatch'>
 		/// Sprite batch.
 		/// </param>
-        public void Draw(SpriteBatch spriteBatch)
+        public virtual void Draw(SpriteBatch spriteBatch)
         {
             var sourceRectangle = SpriteSheetCalculator.CalculateSourceRect((int)Width, (int)Height, columns, rows, currentFrame, moved);
             var destinationRectangle = SpriteSheetCalculator.CalculateDestinationRect(position, sourceRectangle);
@@ -126,6 +127,12 @@ namespace adventure_through_the_knight.Output
         private bool Blocked(Vector2 newPosition)
         {
             var boundingBox = CreateBoundingBoxFromPosition(newPosition);
+            
+            // Check for any collisions with walls.
+            if (CollisionManager.CheckForWallCollison(wallManager, boundingBox) == true)
+            {
+                return true;
+            }
             return !movementBounds.Contains(boundingBox);
         }
 
@@ -230,6 +237,15 @@ namespace adventure_through_the_knight.Output
         public bool InRange(float TestVariable, float Min, float Max)
         {
             return TestVariable < Max && TestVariable >= Min ? true : false; 
+        }
+
+        /// <summary>
+        /// Sets the sprite's wall manager
+        /// </summary>
+        /// <param name="wallManager">the room of walls to check for collisions against</param>
+        public void SetWallManager(Walls.WallManager wallManager)
+        {
+            this.wallManager = wallManager;
         }
 
         /// <summary>
---
+++ adventure-through-the-knight/adventure-through-the-knight/Output/NPC/NPC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adventure_through_the_knight.Output.NPC
+{
+    public class NPC : Sprite
+    {
+
+    }
+}
