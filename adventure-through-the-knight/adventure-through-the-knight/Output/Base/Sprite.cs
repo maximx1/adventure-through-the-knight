9df3bc89347b90bc6e1687509d0e6d58b756c8ef
Justin Walrath <walrathjaw@gmail.com>
2013-01-20 20:41:11 -0500

    Prepped for merge.

--- adventure-through-the-knight/adventure-through-the-knight/Output/Base/Sprite.cs
+++ adventure-through-the-knight/adventure-through-the-knight/Output/Base/Sprite.cs
@@ -228,18 +228,6 @@ namespace adventure_through_the_knight.Output
         }
 
         /// <summary>
-        /// Test if a number is within the range of another number.
-        /// </summary>
-        /// <param name="TestVariable">True if the value is within range</param>
-        /// <param name="Min">The Minimum number inclusive.</param>
-        /// <param name="Max">The Maximum number non-inclusive</param>
-        /// <returns></returns>
-        public bool InRange(float TestVariable, float Min, float Max)
-        {
-            return TestVariable < Max && TestVariable >= Min ? true : false; 
-        }
-
-        /// <summary>
         /// Sets the sprite's wall manager
         /// </summary>
         /// <param name="wallManager">the room of walls to check for collisions against</param>
